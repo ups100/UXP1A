5aaff531aadce8a0ab3465b45a62e54343238660
Kajo0 <kajo100@gmail.com>
2013-04-27 15:26:28 +0200

    disabled doxygen generation Shared::Configuration path to pipe as static string

--- code/Shared/src/Configuration.cpp
+++ code/Shared/src/Configuration.cpp
@@ -1,30 +1,28 @@
-///////////////////////////////////////////////////////////
-//  Configuration.cpp
-//  Implementation of the Class Configuration
-//  Created on:      24-kwi-2013 11:08:34
-//  Original author: kopasiak
-///////////////////////////////////////////////////////////
+/**
+ * @file Configuration.cpp
+ *
+ * @date 27-04-2013
+ *
+ * @author Mikolaj Markiewicz <kajo100@gmail.com>
+ *
+ * @brief Implementation of the Class UXP1A_project::Shared::Configuration
+ *
+ * @par Project
+ * This is a part of project realized on Warsaw University of Technology
+ * on UXP1A lectures.
+ */
 
 #include "Configuration.h"
 
 namespace UXP1A_project {
 namespace Shared {
 
-Configuration::Configuration()
-{
+const char *Configuration::CONFIG_FILE_NAME = "/tmp/.UXP1A_commands";
 
+const QString Configuration::getServerFifoPath()
+{
+    return QString(CONFIG_FILE_NAME);
 }
 
-Configuration::~Configuration()
-{
-
-}
-
-const QString& Configuration::getServerFifoPath()
-{
-
-    return QString();
-}
-
-}//namespace Shared
-}//namesoace UXP1A_project
+} //namespace Shared
+} //namesoace UXP1A_project
--- code/Tests/src/main.cpp
+++ code/Tests/src/main.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
+#include "Configuration.h"
+
+
 int main(int argc, char **argv)
 {
 
+    UXP1A_project::Shared::Configuration c;
+    std::cout<<UXP1A_project::Shared::Configuration::CONFIG_FILE_NAME<<"\n";
+    std::cout<<UXP1A_project::Shared::Configuration::getServerFifoPath().toStdString().c_str()<<"\n";
+
+    std::cout<<"\n";
     return 0;
 }
