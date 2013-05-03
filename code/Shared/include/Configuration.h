760c64bb5367e90bb13dd3f068aa1e9a31d2befc
Jacek Sosnowski <profilJacka@gmail.com>
2013-05-03 18:03:59 +0200

    Add some useful method to Configuration class.

--- code/Shared/include/Configuration.h
+++ code/Shared/include/Configuration.h
@@ -16,6 +16,7 @@
 #define EA_5B26D2EF_5E9E_4623_A7EB_9A8732F16065__INCLUDED_
 
 #include <QString>
+#include <string>
 
 namespace UXP1A_project {
 namespace Shared {
@@ -33,9 +34,13 @@ class Configuration
 public:
 
     /**
-     * @brief Name (path) of pipe for commands
+     * @brief Name of pipe for commands
      */
     static const char *CONFIG_FILE_NAME;
+    /**
+     * @brief Path where pipe is located
+     */
+    static const char *CONFIG_FILE_PATH;
 
     /**
      * @brief Get name (path) of pipe for commands
@@ -43,6 +48,16 @@ public:
      * @return Self defined const name (path) of pipe
      */
     static const QString getServerFifoPath();
+
+    /**
+     * @brief Give only path - directories name where FIFO file exists.
+     */
+    static const std::string getServerFifoDir();
+
+    /**
+     * @brief Give only name of server command FIFO file.
+     */
+    static const std::string getServerFifoName();
 
 };
 
--- code/Shared/src/Configuration.cpp
+++ code/Shared/src/Configuration.cpp
@@ -17,11 +17,24 @@
 namespace UXP1A_project {
 namespace Shared {
 
-const char *Configuration::CONFIG_FILE_NAME = "/tmp/.UXP1A_commands";
+//TODO I remove dot from file name - after we can add it again
+const char *Configuration::CONFIG_FILE_NAME = "UXP1A_commands";
+// remember about slash after directory path below:
+const char *Configuration::CONFIG_FILE_PATH = "/tmp/linda/"; //remember - end with '/'
 
 const QString Configuration::getServerFifoPath()
 {
-    return QString(CONFIG_FILE_NAME);
+    return QString(CONFIG_FILE_PATH) + QString(CONFIG_FILE_NAME);
+}
+
+const std::string Configuration::getServerFifoDir()
+{
+    return std::string(CONFIG_FILE_PATH);
+}
+
+const std::string Configuration::getServerFifoName()
+{
+    return std::string(CONFIG_FILE_NAME);
 }
 
 } //namespace Shared
