ceaf6a051588d0b5630d3d07529cedb071393008
Kajo0 <kajo100@gmail.com>
2013-05-14 18:37:52 +0200

    d

--- code/Tests/src/main.cpp
+++ code/Tests/src/main.cpp
@@ -1,4 +1,7 @@
-#include "ManualLinda.h"
+//#include "ManualLinda.h"
+#include "LindaClient.h"
+#include <QDebug>
+using namespace UXP1A_project::Client;
 
 //void testServerFifo();
 
@@ -6,8 +9,19 @@ int main(int argc, char **argv)
 {
 //    void testServerFifo();
 
-    UXP1A_project::Tests::ManualLinda m;
-    m.start();
+//    UXP1A_project::Tests::ManualLinda m;
+//    m.start();
+
+    if (argc == 1) {
+        // pull int:*
+        QVariantList list;
+        qDebug() << LindaClient::pull("int:*");
+    } else if (argc == 2) {
+        // push int:-1
+        QVariantList list;
+        list << -1;
+        LindaClient::push(list);
+    }
 
     return 0;
 }
