10943e5b16c52e1e132c3c6efd4ac8fa9c1fac14
Kajo0 <kajo100@gmail.com>
2013-05-15 20:03:55 +0200

    only doc ;) now

--- code/Tests/src/main.cpp
+++ code/Tests/src/main.cpp
@@ -8,13 +8,21 @@ int main(int argc, char **argv)
 {
 //    void testServerFifo();
 
-    if (argc < 5)
-        exit(1);
+    int ar[4];
 
-    UXP1A_project::Tests::ManualLinda m(boost::lexical_cast<int>(argv[1]),
-            boost::lexical_cast<unsigned int>(argv[2]),
-            boost::lexical_cast<unsigned int>(argv[3]),
-            boost::lexical_cast<unsigned int>(argv[4]));
+    if (argc < 5) {
+        ar[0] = 0;
+        ar[1] = 0;
+        ar[2] = 0;
+        ar[3] = 0;
+    } else {
+        ar[0] = boost::lexical_cast<int>(argv[1]);
+        ar[1] = boost::lexical_cast<unsigned int>(argv[2]);
+        ar[2] = boost::lexical_cast<unsigned int>(argv[3]);
+        ar[3] = boost::lexical_cast<unsigned int>(argv[4]);
+    }
+
+    UXP1A_project::Tests::ManualLinda m(ar[0], ar[1], ar[2], ar[3]);
     std::cout << m.start().toStdString();
 
     return 0;
